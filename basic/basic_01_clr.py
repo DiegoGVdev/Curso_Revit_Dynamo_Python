666a01d1853a5ba5eae159ee499675a84370fd78
diegogvdev <dagalviz83@soy.sena.edu.co>
2023-07-27 19:38:06 -0500

    feature/initial_basic_01_clr

--- basic/basic_01_clr.py
+++ basic/basic_01_clr.py
@@ -1,1 +1,35 @@
+__Author__ = 'Diego Galviz'
+__License__ = 'Free use'
+__version__ = '1.0'
+__status__ = 'Development'
+
+
+"""
+En el siguiente archivo se muestra como importar un modulo de clr. 
+El modulo clr es un modulo que permite importar librerias de .net a python.
+En este caso se importa la libreria ProtoGeometry.dll que es una libreria de .net
+que contiene las clases de geometria de Dynamo.
+"""
+
+# _________________________________
+# importando modulo clr
+import clr
+
+# Importando archivo dll
+clr.AddReference("ProtoGeometry")
+
+# importando namespace
+from Autodesk.DesignScript.Geometry import *
+
+
+# _________________________________
+# output o salida
+OUT = Point.ByCoordinates.__doc__
+
+
+
+
+
+
+
 
